a84534ebc53f8ddf2acf735ce72d9ab204b16458
Twan Terstappen <32594179+Twan2013@users.noreply.github.com>
2022-12-15 22:55:18 +0100

    Update install_webserver.sh

--- install_webserver.sh
+++ install_webserver.sh
@@ -71,6 +71,9 @@ sudo cat >> /var/www/flask/flask.wsgi << EOF
 #!/usr/bin/python
 import sys
 import logging
+activate_this = '/var/www/flask/flaskr/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/flask/")
 
